b30d106c8db839f64ee280930e0e1f65a68c2d65
Дарья Рутц <darya_r_qschool@qsoft.ru>
2023-12-20 18:00:29 +0300

    изменить путь

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/dist',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/realco-vue.netlify.app/'
+    : '/',
   pwa: {
     workboxOptions: {
       exclude: [/_redirects/]
