aa985578681818715f867cc1dc740033bce6a840
Дарья Рутц <darya_r_qschool@qsoft.ru>
2023-12-20 18:06:37 +0300

    изменить путь

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/realco-vue.netlify.app/dist" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/dist" : "/",
   css: {
     loaderOptions: {
       scss: {
